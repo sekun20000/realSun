005a5359fb6a0f14be4c8568608ec2ed8b7a0a7e
Lenovo <Lenovo@LAPTOP-26O28SC9>
2018-07-29 03:14:27 +0900

    구구단코드

---
+++ src/sunstudy/gugudan.java
@@ -0,0 +1,13 @@
+package sunstudy;
+
+public class gugudan {
+	public static void main(String[] args) {
+		for(int a = 1; a <10 ; a = a+1) {
+			System.out.println( a+"단입니다----------");
+			for(int b =1 ; b <10 ; b = b +1) {
+				System.out.println(a*b);
+			}
+		}
+	}
+	
+}
